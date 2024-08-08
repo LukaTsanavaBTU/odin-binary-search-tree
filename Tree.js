ed402619d2bc500d059c66204768fc26a555a323
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-08-08 16:02:25 +0400

    Add function to sort array and remove duplicates

--- Tree.js
+++ Tree.js
@@ -1,10 +1,16 @@
 import Node from "./Node.js";
 
+function sortArray(array) {
+    const noDuplicates = [...new Set(array)];
+    const sorted = noDuplicates.sort((a, b) => a - b);
+    return sorted;
+}
+
 export default class Tree {
     constructor(array) {
         this.root = this.buildTree(array);
     }
-    // buildTree() {
-
-    // }
+    buildTree(array) {
+        const sorted = sortArray(array);
+    }
 }
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 import Tree from "./Tree.js";
+
+const unsorted = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 1, 1, 1, 1];
+
