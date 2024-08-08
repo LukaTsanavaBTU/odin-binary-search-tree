3ea7e31939c7bbe0dea9487831a975f7d022a882
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-08-08 16:36:37 +0400

    Add default parameters for Node class constructor

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 import Tree from "./Tree.js";
 
-const unsorted = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 1, 1, 1, 1];
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
 
-const bst = new Tree(unsorted);
 
-console.log(bst.root);
+const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// bst.insert(-1);
+prettyPrint(bst.root);
