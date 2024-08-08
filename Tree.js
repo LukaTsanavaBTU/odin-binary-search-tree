39612c7f6000a3eefc08539ba3e7116d5adb1e6a
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-08-08 16:11:45 +0400

    Add buildTree method

--- Tree.js
+++ Tree.js
@@ -5,6 +5,16 @@ function sortArray(array) {
     const sorted = noDuplicates.sort((a, b) => a - b);
     return sorted;
 }
+function recursiveBuild(array, start, end) {
+    if (start > end) {
+        return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(mid);
+    root.left = recursiveBuild(array, start, mid - 1);
+    root.right = recursiveBuild(array, mid + 1, end);
+    return root;
+}
 
 export default class Tree {
     constructor(array) {
@@ -12,5 +22,6 @@ export default class Tree {
     }
     buildTree(array) {
         const sorted = sortArray(array);
+        return recursiveBuild(sorted, 0, sorted.length - 1);
     }
 }
--- main.js
+++ main.js
@@ -2,3 +2,6 @@ import Tree from "./Tree.js";
 
 const unsorted = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 1, 1, 1, 1];
 
+const bst = new Tree(unsorted);
+
+console.log(bst.root);
